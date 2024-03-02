21aec1b23cf4973b7454c63964b583e44771862a
Stepa Shishkin <151217141+b0tLy@users.noreply.github.com>
2024-03-02 17:49:16 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,18 +3,51 @@
 #include "alg.h"
 
 
-bool checkPrime(uint64_t value) {
-// вставьте код функции
+bool checkPrime(uint64_t value){
+    if (value<=1){
+        return false;
+        }
+    if (value==2){
+        return true;
+        }
+    for (int i = 2; i < value; i ++){
+        if (value%i == 0){
+            return false;
+            }
+        }
+return true;
 }
 
-uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+
+uint64_t nPrime(uint64_t n){
+uint64_t counter = 0;
+uint64_t number = 1;
+while (counter < n) {
+    number++;
+    if (checkPrime(number)) {
+        counter++;
+        }
+    }
+return number;
 }
 
-uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+uint64_t nextPrime(uint64_t value){
+uint64_t num = 0;
+for (int i = value + 1; i; i++) {
+    if (checkPrime(i)) {
+        num = i;
+        break;
+    }
+}
+return num;
 }
 
-uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t sumPrime(uint64_t hbound){
+uint64_t num = 0;
+for(uint64_t i=0; i<hbound; i++){
+    if(checkPrime(i)){
+        num+=i;
+    }
+}
+return num;
 }
